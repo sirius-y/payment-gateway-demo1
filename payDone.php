551b3154083f40804e69c3d23ce02711c9a03489
Yashvardhan P Dwivedi <46245660+sirius-y@users.noreply.github.com>
2021-03-18 21:18:15 +0530

    Add files via upload

---
+++ payDone.php
@@ -0,0 +1,75 @@
+<html>
+<title>
+  Payment Success
+</title>
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap" rel="stylesheet">
+    <nav class="navbar">
+    <a href="home.html" class="btn">Home</a>
+       <a href="gateway.html" class="btn">Donate</a>
+       <a href="contact.html" class="btn">Contact</a>
+     </nav>
+  </head>
+    <style>
+      body {
+        text-align: center;
+        padding: 40px 0;
+        background: #EBF0F5;
+      }
+        h1 {
+          color: #88B04B;
+          font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
+          font-weight: 900;
+          font-size: 40px;
+          margin-bottom: 10px;
+        }
+        p {
+          color: #404F5E;
+          font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
+          font-size:20px;
+          margin: 0;
+        }
+      i {
+        color: #9ABC66;
+        font-size: 100px;
+        line-height: 200px;
+        margin-left:-15px;
+      }
+      .card {
+        background: white;
+        padding: 60px;
+        border-radius: 4px;
+        box-shadow: 0 2px 3px #C8D0D8;
+        display: inline-block;
+        margin: 0 auto;
+      }
+      .btn{
+        color:#999;
+        background:rgba(0, 0, 0, 0.5);
+        padding:10px 20px;
+        font-size:12px;
+        text-decoration:none;
+        letter-spacing:2px;
+        text-transform:uppercase;
+      }
+
+      .btn:hover{
+        border:none;
+        background:rgba(0, 0, 0, 0.4);
+        background:#fff;
+        padding:20px 20px; #000;
+        color:#1b1b1b;
+      }
+    </style>
+    <body>
+      <div class="card">
+      <div style="border-radius:200px; height:200px; width:200px; background: #F8FAF5; margin:0 auto;">
+        <i class="checkmark">✓</i>
+      </div>
+        <h1>Success</h1>
+        <p>Thank You for the Contribution to this noble Cause.<br/> It will go a long way for the ones who will be benefitted from this gift!
+          <br/>Payment Invoice is mailed to you.</p>
+        <p>Please use the Navigation Bar above to navigate the website.</p>
+      </div>
+    </body>
+</html>
